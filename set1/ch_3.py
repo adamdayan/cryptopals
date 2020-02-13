4aff1492f6c7d61bb117bec00a2d9c282c11b2e3
Adam Dayan <adam.dayan@garrisontechnology.com>
2020-02-13 18:08:52 +0000

    adding incomplete challenge 4 answer

--- set1/ch_3.py
+++ set1/ch_3.py
@@ -30,7 +30,8 @@ def xor_single_byte(target_bytes, key):
 
 def decrypt(target_bytes, key):
     decrypted_target_bytes = xor_single_byte(target_bytes, key)
-    decrypted_string = decrypted_target_bytes.decode("utf-8", errors="ignore")
+    decrypted_string = decrypted_target_bytes.decode("ascii")
+        
     return decrypted_string
 
 def compute_loss(target_frequency, reference_frequency):
@@ -47,7 +48,10 @@ def compute_likely_key(target_hex, reference_frequency):
     cur_best_key = -1
 
     for key in range(256):
-        decrypted_target = decrypt(bytes.fromhex(target_hex), key)
+        try:
+            decrypted_target = decrypt(bytes.fromhex(target_hex), key)
+        except UnicodeDecodeError:
+            continue
         target_frequency = compute_letter_freq(decrypted_target)
         loss = compute_loss(target_frequency, reference_frequency)
         
@@ -55,21 +59,22 @@ def compute_likely_key(target_hex, reference_frequency):
             cur_best_loss = loss 
             cur_best_key = key
 
-    return cur_best_key
+    return cur_best_key, cur_best_loss
 
 
-def crack_single_byte_cypher(target_hex, sample_text_path):
+def crack_single_byte_cipher(target_hex, sample_text_path):
     with open(sample_text_path, "r") as t:
         sample_text = t.read()
 
     reference_frequency = compute_letter_freq(sample_text)
 
-    most_likely_key = compute_likely_key(target_hex, reference_frequency)
+    most_likely_key, most_likely_loss = compute_likely_key(target_hex, reference_frequency)
 
+    print("Target hex: {}".format(target_hex))
     print("Most likely key: {}".format(most_likely_key))
     print("Resulting decrypt: {}".format(decrypt(bytes.fromhex(target_hex), most_likely_key)))
 
 
 if __name__=="__main__":     
     intercept = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-    crack_single_byte_cypher(intercept, "sample_text.txt")
+    crack_single_byte_cipher(intercept, "sample_text.txt")
---
+++ set1/ch_4.py
@@ -0,0 +1,44 @@
+import string
+import math
+import ch_3 as sbk
+
+def find_minimum_loss_with_single_byte_key(target_list, reference_frequency):
+    cur_best_loss = math.inf
+    cur_best_idx = -1
+    cur_best_decrypt = ""
+
+    for idx, target in enumerate(target_list):
+        key, loss = sbk.compute_likely_key(target, reference_frequency)
+        if loss < cur_best_loss:
+            cur_best_loss = loss 
+            cur_best_idx = idx
+            cur_best_decrypt = sbk.decrypt(
+                bytes.fromhex(target)
+                , key
+            )
+
+            print(target, target_list[cur_best_idx], key, cur_best_decrypt)
+
+    return target_list[cur_best_idx], cur_best_decrypt, cur_best_loss
+
+
+def find_single_byte_cipher_string(target_path, sample_text_path):
+    with open(target_path, "r") as targ:
+        target_list = targ.read().splitlines()
+
+    with open(sample_text_path, "r") as txt:
+        sample_text = txt.read()
+
+    reference_frequency = sbk.compute_letter_freq(sample_text)
+
+    single_byte_target, single_byte_decrypt, single_byte_loss = find_minimum_loss_with_single_byte_key(
+        #[t for t in target_list.__reversed__()],
+        target_list,
+        reference_frequency
+    )
+
+    print("Single byte key encrypted string: {}".format(single_byte_target))
+    print("Single byte key decrypted string: {}".format(single_byte_decrypt))
+
+if __name__=="__main__":
+    find_single_byte_cipher_string("data_ch_4.txt", "sample_text.txt")
