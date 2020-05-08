bf4e2dc2dccb1ae45f679daa22d1583d829ef682
nihao10 <nihao10@gmail.com>
2020-05-08 18:51:05 +0100

    detecting CBC vs ECB

--- set2/ch_1.py
+++ set2/ch_1.py
@@ -1,11 +1,15 @@
 def padder(target_bytearray, block_size):
+    if len(target_bytearray) == block_size:
+        return target_bytearray
     num_missing_bytes = block_size - (len(target_bytearray) % block_size)
     missing_bytes = bytearray([num_missing_bytes]) * num_missing_bytes
     return target_bytearray + missing_bytes
 
 def unpadder(padded_bytearray):
     potential_missing_byte_num = padded_bytearray[-1]
-    for i in range(len(padded_bytearray) - 1, len(padded_bytearray) - potential_missing_byte_num):
+    if potential_missing_byte_num == 0:
+        return padded_bytearray
+    for i in range(len(padded_bytearray) - 1, len(padded_bytearray) - potential_missing_byte_num, -1):
         if padded_bytearray[i] != potential_missing_byte_num:
             return padded_bytearray
 
@@ -21,6 +25,6 @@ if __name__=="__main__":
     
     padded_target_bytearray = padder(target_bytearray, block_size)
     print("Target '{}' of length {} padded to a block size of {} results in '{}'".format(target_bytearray, len(target_bytearray), block_size, padded_target_bytearray))
-
+    
     unpadded_target_bytearray = unpadder(padded_target_bytearray)
     print(unpadded_target_bytearray)
---
+++ set2/ch_3.py
@@ -0,0 +1,60 @@
+import random
+
+from set2.ch_2 import encrypt as encrypt_cbc
+from set2.ch_1 import padder, unpadder
+from Crypto.Cipher import AES
+
+def gen_random_key(key_size):
+    return bytearray([random.randint(0, 255) for k in range(key_size)])
+
+def prepender_appender(target_bytearr):
+    prepend_cnt = random.randint(5, 10)
+    append_cnt = random.randint(5, 10)
+
+    target_bytearr = bytearray([random.randint(0, 255) for k in range(prepend_cnt)]) + target_bytearr
+    target_bytearr = target_bytearr + bytearray([random.randint(0, 255) for k in range(append_cnt)])
+
+    return target_bytearr
+
+def encrypt_ecb(target_bytearr, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    return cipher.encrypt(target_bytearr)
+
+def encrypt_ecb_cbc(target_bytearr, is_ecb):
+    key = gen_random_key(16)
+    padded_target_bytearr = padder(target_bytearr, len(key))
+    if is_ecb:
+        ciphertext_bytearr = encrypt_ecb(bytes(padded_target_bytearr), bytes(key))
+    else:
+        iv = bytearray([random.randint(0, 255) for k in range(len(key))])
+        ciphertext_bytearr = encrypt_cbc(bytes(padded_target_bytearr), iv, bytes(key))
+
+    return unpadder(ciphertext_bytearr)
+
+
+def identify_ecb(is_ecb):
+    for key_size in range(1, 32):
+        target_bytearr = bytearray([33]) * 100
+        target_bytearr = prepender_appender(target_bytearr)
+        ciphertext_bytearr = encrypt_ecb_cbc(target_bytearr, is_ecb)
+        if ciphertext_bytearr[40:45] == ciphertext_bytearr[40 + key_size:45 + key_size]:
+            return True
+
+    return False
+
+
+if __name__=="__main__":
+    is_ecb = random.randint(0, 1)
+    print("is_ecb: ", is_ecb) 
+    
+    guess_is_ecb = identify_ecb(is_ecb)
+
+    assert guess_is_ecb == is_ecb, "INCORRECT CIPHER ID"
+    
+    if is_ecb: 
+        print("CORRECT CIPHER ID: ECB")
+    else: 
+        print("CORRECT CIPHER ID: CBC")
+        
+    
+    
