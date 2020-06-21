393aa9ec86cd5c0c466054ead414af50326cae47
nihao10 <nihao10@gmail.com>
2020-06-21 23:52:10 +0100

    completed ch 14

--- set2/ch_6.py
+++ set2/ch_6.py
@@ -52,10 +52,10 @@ def find_prefix_len(prefix_bytearr, append_bytearr, key, keysize):
 
     return last_unchanged_block + keysize 
 
-def detect_ecb(prefix_bytearr, append_bytearr, key, keysize):
-    target_bytearr = bytearray([35]) * (keysize * 2)
+def detect_ecb(prefix_bytearr, prefix_len, prefix_supplement_bytes, append_bytearr, key, keysize):
+    target_bytearr = bytearray([35]) * ((keysize * 2) + prefix_supplement_bytes)
     cipher_bytearr = encrypt(prefix_bytearr, target_bytearr, append_bytearr, key)
-    if cipher_bytearr[-(2 * keysize):-keysize] == cipher_bytearr[-keysize:]:
+    if cipher_bytearr[prefix_len + prefix_supplement_bytes:prefix_len + prefix_supplement_bytes + keysize] == cipher_bytearr[prefix_len + prefix_supplement_bytes + keysize:prefix_len + prefix_supplement_bytes + (keysize * 2)]:
         return True
     return False
 
@@ -68,11 +68,50 @@ def generate_random_bytes():
 
     return random_bytes
 
+def generate_missing_byte_dict(prefix_bytearr, prefix_bytes_supplement, prefix_len, known_bytes, append_bytes, key, keysize):
+    missing_byte_dict = {}
+    prefix_supplement_bytes = prefix_bytes_supplement * bytearray([97])
+    for missing_byte in range(256):
+        cipher_bytearr = encrypt(prefix_bytearr, prefix_supplement_bytes + known_bytes + bytearray([missing_byte]), append_bytes, key) 
+        block_of_interest = cipher_bytearr[prefix_len + prefix_bytes_supplement : prefix_len + prefix_bytes_supplement + keysize]
+        missing_byte_dict[bytes(block_of_interest)] = bytearray([missing_byte])
+
+    return missing_byte_dict
+
+def attack_ecb(prefix_bytearr, append_bytearr, key):
+    keysize = find_keysize(prefix_bytearr, append_bytearr, key)
+    prefix_len = find_prefix_len(prefix_bytearr, append_bytearr, key, keysize)
+    prefix_nearest_block_supplement = keysize - (prefix_len % keysize)
+
+    if not detect_ecb(prefix_bytearr, prefix_len , prefix_nearest_block_supplement,append_bytearr, key, keysize):
+        raise Exception("Not ECB - cannot decrypt!")
+
+    uncovered_bytes = bytearray()
+    for cnt, byte in enumerate(append_bytearr):
+        num_attack_bytes= keysize- (cnt % keysize) - 1 + prefix_nearest_block_supplement
+        attack_bytes = bytearray([97]) * num_attack_bytes
+        known_bytes = attack_bytes + uncovered_bytes
+        missing_byte_dict = generate_missing_byte_dict(prefix_bytearr, 
+                                                       prefix_nearest_block_supplement, 
+                                                       prefix_len, 
+                                                       known_bytes[-(keysize - 1):],
+                                                       append_bytearr,
+                                                       key, 
+                                                       keysize)
+        cipher_bytearr = encrypt(prefix_bytearr, attack_bytes, append_bytearr, key)
+        lookup_bytes = cipher_bytearr[len(known_bytes) + prefix_len + 1 - keysize: len(known_bytes) + prefix_len + 1]
+        missing_byte = missing_byte_dict[bytes(lookup_bytes)]
+        uncovered_bytes += missing_byte
+
+    return uncovered_bytes
+
+
 if __name__=="__main__":
     key = "YELLOW SUBMARINE".encode("utf-8")
     
     prefix_bytearr = generate_random_bytes()
-    append_bytearr = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK".encode("utf-8")
+    append_string= "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
+    append_bytearr = base64.b64decode("".join(append_string.strip().split("\n")))
 
     keysize = find_keysize(prefix_bytearr, append_bytearr, key)
     assert keysize == len(key), "Incorrect keysize!"
@@ -80,4 +119,6 @@ if __name__=="__main__":
     prefix_len = find_prefix_len(prefix_bytearr, append_bytearr, key, keysize)
     assert prefix_len == len(prefix_bytearr), "Incorrect prefix length. Computed: {} Actual: {}".format(prefix_len, len(prefix_bytearr))
 
-
+    
+    plaintext = attack_ecb(prefix_bytearr, append_bytearr, key)
+    print("Decrypted plaintext: ", plaintext)
